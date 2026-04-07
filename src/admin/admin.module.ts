48603a88cd53138d438f9fa705cde7bda093cdb3
Aymen Ben Naser <aymenpower99@gmail.com>
2026-04-07 19:56:52 +0100

    Updated admin.module.ts file with new code

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -4,13 +4,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { AdminService }    from './admin.service';
 import { AdminController } from './admin.controller';
 import { User }    from '../users/entites/user.entity';
-import { Driver }  from '../driver/entities/driver.entity';  // ← NEW
+import { Driver }  from '../driver/entities/driver.entity';
+import { PassengerEntity } from '../passenger/entities/passengers.entity';
 import { MailModule }    from '../mail/mail.module';
 import { HtmlService }   from '../common/services/html.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Driver]),  // ← Driver added
+    TypeOrmModule.forFeature([User, Driver, PassengerEntity]),
     JwtModule.register({}),
     MailModule,
   ],
